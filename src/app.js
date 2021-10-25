69b7bc6ae495d9c54e45162d760619c0d0143c58
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2021-10-25 16:44:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const getSS= async(url)=> {
     try {
         const browser = await puppeteer.launch({
-            args : ['--no-sandbox']
+            args : ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
         await page.setDefaultNavigationTimeout(0);

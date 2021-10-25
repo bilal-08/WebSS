2327dc240593a4eddea99d1aa91921594713315d
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2021-10-25 16:44:59 +0530

    Update pdf.js

--- src/pdf.js
+++ src/pdf.js
@@ -5,7 +5,7 @@ const getPDF = async(url) =>{
 
     try {
             const browser = await puppeteer.launch({
-                args : ['--no-sandbox']
+                args : ['--no-sandbox', '--disable-setuid-sandbox'  ]
             });
             const page = await browser.newPage();
             await page.goto(`http://${url}`, {
